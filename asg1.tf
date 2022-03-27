ec5d354c0a62d8c396c185b90d32a9935eebf459
pavanikommi <99424545+pavanikommi@users.noreply.github.com>
2022-03-27 23:58:58 +0530

    Update asg1.tf

--- asg1.tf
+++ asg1.tf
@@ -3,11 +3,11 @@ image_id = "ami-0bbf29c4ca3d5addb"
 instance_type = "t2.micro"
 }
 resource "aws_autoscaling_group" "example" {
-  ##launch_configuration = ["terraform-20220325123959001600000001"]
-  ##launchtemplate_id = var.lt
+  
+  
   availability_zones   = ["${var.availability_zone}"]
   min_size = var.min_size
   max_size = var.max_size
-  # (...)
+  
   launch_configuration = "lconfig"
 }
