cfe99dc890e14a9cd55576692a38a05095b5152e
pavanikommi <99424545+pavanikommi@users.noreply.github.com>
2022-03-29 23:06:13 +0530

    Update asg1.tf

--- asg1.tf
+++ asg1.tf
@@ -5,7 +5,7 @@ instance_type = "t2.micro"
 resource "aws_autoscaling_group" "example" {
   
  
-  avilability_zones = ["us-east-1"]
+  availability_zones = ["us-east-1"]
   min_size = 2
   max_size = 5
   launch_configuration = "lconfig"
