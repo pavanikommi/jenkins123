b2ca31c207e54ae4967ce1f6887022ce9516c224
pavanikommi <99424545+pavanikommi@users.noreply.github.com>
2022-03-29 22:24:44 +0530

    Update asg1.tf

--- asg1.tf
+++ asg1.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_configuration" "example" {
-image_id = "ami-0bbf29c4ca3d5addb"
+image_id = "ami-0ff843af0e81f1886"
 instance_type = "t2.micro"
 }
 resource "aws_autoscaling_group" "example" {
